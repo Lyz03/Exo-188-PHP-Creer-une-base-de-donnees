bafaf0ee6cf18cc89197b940d9850a7ad3a0e0f7
Lyz03 <lizoe@netcourrier.com>
2022-02-09 13:29:42 +0100

    exo 188

--- index.php
+++ index.php
@@ -23,13 +23,13 @@
 // TODO Votre code ici bas.
 
 try {
-    $maConnexion = ........
+    $maConnexion = new PDO("mysql:host=localhost;charset=utf8", 'root', '');
 
     $request = "
-        Ma super requête SQL pour créer une base de données.
+        CREATE DATABASE intro_sql
     ";
 
-    $maConnexion->une super méthode pour exécuter ma requete
+    $maConnexion->exec($request);
 
     echo "La base de données intro_sql a bien été créée.";
 }
@@ -39,11 +39,33 @@ catch (PDOException $exception) {
 
 
 
+try {
+
+    $maConnexion->exec("DROP DATABASE intro_sql");
+
+    echo "La base de données intro_sql a bien été supprimée.";
+}
+catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
 
 
 
 
+try {
+    $maConnexion = new PDO("mysql:host=localhost;charset=utf8", 'root', '');
 
+    $request = "
+        CREATE DATABASE intro_sql
+    ";
+
+    $maConnexion->exec($request);
+
+    echo "La base de données intro_sql a bien été créée.";
+}
+catch (PDOException $exception) {
+    echo $exception->getMessage();
+}
 
 
 
